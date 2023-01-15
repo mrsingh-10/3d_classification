f96714241519cfddad81129e55273275a76b2a7a
Harjot Singh <harjot.98.singh@gmail.com>
2023-01-15 13:02:54 +0100

    Removed unused code,
    no need for scaling all done in voxelizing step
    no need for clustering to simplify to few voxels

--- code/Voxelizer_SingleMesh.py
+++ code/Voxelizer_SingleMesh.py
@@ -6,28 +6,13 @@ PADDING = 0 # PADDING is added twice to the right
 
 outputDirName = "Test"
 isTestModel = False
-model = "sofa_0001" #"table_0116" #ok with /64 and "table_0361" ok only with export with pcd
+model = "sofa_0001"
 
 current_inputModel, current_outputModel = Helper.getFoldersFromModel(model,isTestModel, outputDirName)
 print(current_inputModel,current_outputModel)
 
-# checking if the input file is present
-#print("IS FILE PRESENT:",os.path.isfile(current_inputModel))
-
 # importing the mesh
 mesh = Helper.importMesh(current_inputModel)#.compute_vertex_normals()
-
-# No need for scaling here, everting is done in voxelization
-#mesh.scale(max(mesh.get_max_bound() - mesh.get_min_bound())/SIZE, mesh.get_center())
-
-# getting the voxel size required OVERPASSES
-# voxel_size = max(mesh.get_max_bound() - mesh.get_min_bound()) / Helper.VOXEL_SIZE_DIVIDER
-# print(f'maxBound={mesh.get_max_bound()}, minBound={mesh.get_min_bound()}, center={mesh.get_center()}')
-# print(f'voxel_size = {voxel_size:e}')
-# print(f'Input mesh has {len(mesh.vertices)} vertices and {len(mesh.triangles)} triangles')
-
-# mesh_smp = mesh.simplify_vertex_clustering(voxel_size=voxel_size,contraction=o3d.geometry.SimplificationContraction.Average)
-# print(f'Simplified mesh has {len(mesh_smp.vertices)} vertices and {len(mesh_smp.triangles)} triangles')
 
 # GET & SHOW POINT CLOUD
 pcd = Helper.getPointCloudFromMesh(mesh)
