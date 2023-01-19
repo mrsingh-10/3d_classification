0020ab1a627167cabc23fce9160ddeb786ccff6d
Harjot Singh <harjot.98.singh@gmail.com>
2023-01-19 15:04:27 +0100

    Combined all rotation in one matrix

--- code/TestingRotation.py
+++ code/TestingRotation.py
@@ -58,8 +58,30 @@ def getVGRotateOnX(voxelGrid, rx):
 
     return Helper.getVoxelGridFromArray(R.dot(grid_index_array.T).T)
 
+def getVGRotated(voxelGrid, rx=0,ry=0,rz=0):
+    grid_index_array = np.array(list(map(lambda x:x.grid_index,voxelGrid.get_voxels())))
+    
+    # getting rotation matrix see https://en.wikipedia.org/wiki/Rotation_matrix
+    R = np.eye(3) # ID matrix
+    
+    a = rz
+    b = ry
+    g = rx
+    R[0, 0] = np.cos(a)*np.cos(b)
+    R[0, 1] = np.cos(a)*np.sin(b)*np.sin(g)-np.sin(a)*np.cos(g)
+    R[0, 2] = np.cos(a)*np.sin(b)*np.cos(g)+np.sin(a)*np.sin(g)
+    R[1, 0] = np.sin(a)*np.cos(b)
+    R[1, 1] = np.sin(a)*np.sin(b)*np.sin(g)+np.cos(a)*np.cos(g)
+    R[1, 2] = np.sin(a)*np.sin(b)*np.cos(g)-np.cos(a)*np.sin(g)
+    R[2, 0] = -np.sin(b)
+    R[2, 1] = np.cos(b)*np.sin(g)
+    R[2, 2] = np.cos(b)*np.cos(g)
+    
+    return Helper.getVoxelGridFromArray(R.dot(grid_index_array.T).T)
+
 current_inputModel,current_outputModel = Helper.getFoldersFromModel(model,isTestModel,"Output_v3")
 voxelGrid = Helper.importVoxelGrid(current_outputModel)
 Helper.show(voxelGrid)
 
-Helper.show(getVGRotateOnY(voxelGrid,np.pi/2))
+Helper.show(getVGRotated(voxelGrid,rz=np.pi/2))
+Helper.show(getVGRotateOnZ(voxelGrid,np.pi/2))
