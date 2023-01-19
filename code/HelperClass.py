58ba90aa383bc95c7a0091f6d95144a2b4d5787a
Harjot Singh <harjot.98.singh@gmail.com>
2023-01-19 15:12:18 +0100

    Moved working getVGRotated to HelperClass

--- code/HelperClass.py
+++ code/HelperClass.py
@@ -83,6 +83,28 @@ class HelperClass:
             o3d.visualization.draw_geometries(view)
     
     @classmethod
+    def getVGRotated(cls,voxelGrid, rx=0,ry=0,rz=0):
+        grid_index_array = np.array(list(map(lambda x:x.grid_index,voxelGrid.get_voxels())))
+        
+        # getting rotation matrix see https://en.wikipedia.org/wiki/Rotation_matrix
+        R = np.eye(3) # ID matrix, can also use np.zeros((3,3))
+        
+        a = rz
+        b = ry
+        g = rx
+        R[0, 0] = np.cos(a)*np.cos(b)
+        R[0, 1] = np.cos(a)*np.sin(b)*np.sin(g)-np.sin(a)*np.cos(g)
+        R[0, 2] = np.cos(a)*np.sin(b)*np.cos(g)+np.sin(a)*np.sin(g)
+        R[1, 0] = np.sin(a)*np.cos(b)
+        R[1, 1] = np.sin(a)*np.sin(b)*np.sin(g)+np.cos(a)*np.cos(g)
+        R[1, 2] = np.sin(a)*np.sin(b)*np.cos(g)-np.cos(a)*np.sin(g)
+        R[2, 0] = -np.sin(b)
+        R[2, 1] = np.cos(b)*np.sin(g)
+        R[2, 2] = np.cos(b)*np.cos(g)
+        
+        return cls.getVoxelGridFromArray(R.dot(grid_index_array.T).T)
+        
+    @classmethod
     def getFoldersFromModel(cls,model, isTestModel, outputDirName):
         baseDIR = os.path.dirname(__file__)
         test = isTestModel
