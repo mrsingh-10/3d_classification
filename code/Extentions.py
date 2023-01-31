828456e73bdec42671cd4ebd8a1a35639e68e14f
Harjot Singh <harjot.98.singh@gmail.com>
2023-01-31 21:26:37 +0100

    Added Folderized variant of the getKPartitions m..

--- code/Extentions.py
+++ code/Extentions.py
@@ -11,7 +11,7 @@ VOXEL_GRID_SIZE = 32
 outputFolderName = "Output_ROTATED_v4"
 
 
-def getKPartitions(folderName, K=5):
+def getKPartitionsGathered(folderName, K=5):
     # dooing for all the models
     baseDIR = os.path.dirname(__file__)
     rootModelsDirName = os.path.join(baseDIR, folderName)
@@ -67,8 +67,61 @@ def getKPartitions(folderName, K=5):
     print(f"Randoms: {randomsGlobal}")
     return k_sets
 
+def getKPartitionsFolderized(folderName, K, folderTest):
+    # dooing for all the models
+    baseDIR = os.path.dirname(__file__)
+    rootModelsDirName = os.path.join(baseDIR, folderName)
 
-sets = getKPartitions(outputFolderName, 5)
+    models = ["bathtub", "bed", "chair", "desk", "dresser",
+              "monitor", "night_stand", "sofa", "table", "toilet"]
+    # models = ["desk"]
+
+    # PUSHING ALL models in allModels
+    allModels = {}
+
+    for modelFolder in models:
+        # print(f'current modelFolderName= {modelFolder} and isTestFolder={isTestModel}')
+        # PRELIMINARY STEPS for getting the input folder and creating respective output folder
+
+        # 1) Getting INPUT FILES
+        inputDIR = os.path.join(
+            rootModelsDirName, modelFolder, "test" if folderTest else "train")
+        # print(f'Working on {modelFolder} in folder {inputDIR}')
+        # print(os.path.isdir(inputDIR))
+
+        # Getting the list of all mesh in the directory 'modelFolder'
+        inputModels = []
+        INPUT_EXTENTION = ".ply"
+        # Iterate directory
+        for path in os.listdir(inputDIR):
+            # check if current path is an expected file
+            if os.path.isfile(os.path.join(inputDIR, path)) and os.path.join(inputDIR, path).endswith(INPUT_EXTENTION):
+                # append only the file name
+                inputModels.append(os.path.splitext(path)[0])
+        # print(2, inputModels)
+        allModels[modelFolder] = inputModels
+
+    # DIVIDING allModels into K Partitions
+    k_sets_indexes = [i for i in range(0, K)]
+    randomsGlobal = [0 for _ in range(0, K)]
+
+    # array di K dictionaries inizializzati a empty
+    k_sets = [{} for _ in range(0, K)]
+    for key in allModels:
+        print(key, len(allModels[key]))
+        for v in allModels[key]:
+            # of the bucket
+            index = sample(k_sets_indexes, 1)[0]
+            if not k_sets[index].keys().__contains__(key):
+                k_sets[index][key] = []
+            k_sets[index][key].append(v)
+            # print(index)
+            randomsGlobal[index] += 1
+
+    print(f"Randoms: {randomsGlobal}")
+    return k_sets
+
+sets = getKPartitionsGathered(outputFolderName, 5)
 
 for set in sets:
     for elem in set[:10]:
@@ -76,3 +129,9 @@ for set in sets:
     print("------\n")
 
 print([len(s) for s in sets])
+
+sets = getKPartitionsFolderized(outputFolderName, 5, False)
+
+for idx, set in enumerate(sets):
+    print(idx, len(set["bed"]))
+
